e1a7cf6c5eb20760a23f58d402240456f8ec1291
Thanh <114771414+thanhnm3@users.noreply.github.com>
2025-04-16 08:03:46 +0000

    Cai thien Norm, Toi uu cdc ( con Operation Delete Consumer)

--- service/src/main/java/com/cdc/Maincdc.java
+++ service/src/main/java/com/cdc/Maincdc.java
@@ -4,23 +4,28 @@ package com.cdc;
 
 public class Maincdc {
     public static void main(String[] args) {
+        int producerThreadPoolSize = 2; // Số lượng thread cho Producer
+        int consumerThreadPoolSize = 4; // Số lượng thread cho Consumer
+        int randomOperationsThreadPoolSize = 2; // Số lượng thread cho RandomOperations
+        int maxMessagesPerSecond = 5000; // Giới hạn số lượng message mỗi giây
+        int operationsPerSecond = 500; // Số lượng thao tác mỗi giây cho RandomOperations
+
         // Tạo luồng để chạy AerospikeRandomOperations
         Thread randomOperationsThread = new Thread(() -> {
             System.out.println("Starting AerospikeRandomOperations...");
-            RandomOperations.main(new String[]{}); // Khởi chạy ứng dụng
+            RandomOperations.main(new String[]{}, operationsPerSecond, randomOperationsThreadPoolSize); // Truyền tham số
         });
 
         // Tạo luồng để chạy AerospikePoller
         Thread pollerThread = new Thread(() -> {
-            System.out.println("Starting AerospikePoller...");
-            CdcProducer.main(new String[]{}); // Khởi chạy ứng dụng
+            System.out.println("Starting CdcProducer...");
+            CdcProducer.main(new String[]{}, producerThreadPoolSize); // Truyền threadPoolSize
         });
 
         // Tạo luồng để chạy CdcConsumer
         Thread consumerThread = new Thread(() -> {
             System.out.println("Starting CdcConsumer...");
-            CdcConsumer.main(new String[]{}, 1, 5000); 
-            System.out.println("==========================="); // In ra dấu phân cách sau mỗi lần chạy
+            CdcConsumer.main(new String[]{}, consumerThreadPoolSize, maxMessagesPerSecond); // Truyền threadPoolSize và maxMessagesPerSecond
         });
 
         // Bắt đầu các luồng
